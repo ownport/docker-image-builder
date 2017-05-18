acbb5a11c0ecc32dd070283925dc5c1707b0b073
ownport <ownport@gmail.com>
2017-05-18 21:40:24 +0300

    [+] samples

--- examples/simple-build.py
+++ examples/simple-build.py
@@ -1,5 +1,13 @@
 from __future__ import (absolute_import, division, print_function)
 
-def run(ctxt):
-    for ret in ctxt.cmd(*[ 'apk update', 'apk add python']):
-        print(ret)
+import os
+from pprint import pprint
+
+def run(ctx, **kwargs):
+    # vars = ctx.facts()
+    # print(vars)
+    # ctx.shell('echo "hosts: files dns" >> /etc/nsswitch.conf')
+    # print(ctx.inspect('.NetworkSettings.Networks.bridge.IPAddress'))
+    pprint(ctx.inspect('.NetworkSettings.Networks'))
+    ctx.copy('README.md', '/tmp/')
+    print(ctx.cmd('ls -l /tmp'))
